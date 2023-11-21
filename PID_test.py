b433f5a5a360bc9d01b5e8b98a3a5b7a78f3475a
SeongUk18 <adulty22@gmail.com>
2023-11-22 01:14:39 +0900

    PID_ver3_hsu

--- PID_test.py
+++ PID_test.py
@@ -1,3 +1,6 @@
+import random
+import sensor
+
 class PIDController:
     def __init__(self, kp, ki, kd):
         self.kp = kp
@@ -16,7 +19,7 @@ class PIDController:
         if output < 0:
             output = output * - 1
         output = max(0, min(output, 50))
-        return output
+        return int(output)
 
 # 예시 파라미터
 kp, ki, kd = 2.0, 0.2, 0.05
@@ -25,8 +28,15 @@ pid = PIDController(kp, ki, kd)
 setpoint = 150  # 목표 총량을 150으로 설정
 
 total = 150  # 초기 총량
-input_flows = [random.randint(20, 40) for _ in range(10)]  # 입력 유량의 예시 시퀀스
+# input_flows = [random.randint(20, 40) for _ in range(10)]  # 입력 유량의 예시 시퀀스
+input_flows = []
+output_flows = []
+# for i in range(50):
+#     input_flows.append(sensor.FlowSensor().get_value())
+for i in range(50):
+    input_flows.append(random.randint(0, 30))
 
+print(input_flows)
 for input_flow in input_flows:
     # 입력 유량에 따라 total 업데이트
     total += input_flow
@@ -34,7 +44,13 @@ for input_flow in input_flows:
     # PID 컨트롤러를 이용하여 total 조정
     output_flow = pid.update(setpoint, total)
 
+    if total - output_flow <= 100:
+        output_flow = 0
     # 출력 유량을 이용하여 total 조정
     total -= output_flow
 
     print(f"Input: {input_flow}, Total: {total}, Output Flow: {output_flow}")
+    output_flows.append(output_flow)
+
+print(input_flows)
+print(output_flows)
